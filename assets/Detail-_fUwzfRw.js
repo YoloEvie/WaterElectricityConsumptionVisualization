ee62d0aa2b3c71bc20d6f0410770b5a17ba79ecd
Evie-yii <1719854949@qq.com>
2025-05-24 15:49:43 +0800

    init

--- assets/Detail-_fUwzfRw.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as t,o as r,r as n}from"./index-BBIFGZDO.js";const c={name:"Detail"};function s(a,_,p,i,f,l){const e=n("router-view");return r(),t(e)}const u=o(c,[["render",s]]);export{u as default};
