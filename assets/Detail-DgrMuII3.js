bf3f2a6a90cabfe35b0ecf673ff1a85d6e1bc87d
Evie-yii <1719854949@qq.com>
2025-05-24 15:38:10 +0800

    init

--- assets/Detail-DgrMuII3.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as t,o as r,r as n}from"./index-Dodhh3Br.js";const c={name:"Detail"};function s(a,_,p,i,f,l){const e=n("router-view");return r(),t(e)}const u=o(c,[["render",s]]);export{u as default};
